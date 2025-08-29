6d6e70a3e024f3fea2d05cade37d674e54c22ae2
ahmedz6yed <ahmedz6yed@gmail.com>
2025-08-29 16:43:57 +0300

    Navbar Created

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,21 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResuMate" },
+    { name: "description", content: "Get a real feedback for ur CV" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover ">
+      <Navbar />
+      <section className="main-section">
+        <h1 className="text-4xl font-bold mb-4">Welcome to ResuMate</h1>
+        <p className="text-lg text-gray-700">Get real feedback for your CV</p>
+      </section>
+    </main>
+  );
 }
